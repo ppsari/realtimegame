3f485cabfd15fa1630cf3e926d60b05570f04089
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-06-08 20:50:22 +0700

    add client-side - login & register page

---
+++ quiz/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: {
+
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Open Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>

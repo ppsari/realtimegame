4b263cd6ef5e21c5a53ff6410d50bfbb6caabfc6
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-06-09 14:45:04 +0700

    tampilan static

---
+++ client/quiz/src/components/ListPlayer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list-player">
+    <h6><strong>Active Player</strong></h6>
+    <ul class="collection">
+      <li class="collection-item avatar">
+        <img src="../assets/avatar.png" alt="" class="circle">
+        <span class="title">John Doe</span>
+        <p><small>Total Score: <strong>640</strong></small></p>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  name: 'list-player',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .collection .collection-item.avatar {
+    min-height: 70px;
+  }
+</style>
---
+++ client/quiz/src/components/QuizeCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="quiz-card">
+    <div class="card horizontal">
+      <div class="card-image">
+        <h1 class="center-align"><strong>24</strong></h1>
+        <p class="center-align date"><small>October 2017</small></p>
+        <p class="center-align time"><small>Start At: 11:20 AM</small></p>
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <h5><strong>Quiz Hacktiv - Film</strong></h5>
+          <p><small>Entertainment: Music</small></p>
+        </div>
+        <div class="card-action">
+          <a href="#"><small>Join This Quiz</small></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  name: 'quiz-card',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  margin-bottom: 0;
+}
+.card-image {
+  min-width: 120px;
+  border-right: #c5c5c5 1px solid;
+}
+p.date{
+  margin-top: 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+p.time {
+  margin: 0;
+  padding: 12px;
+  background-color: #c5c5c5;
+  color: white;
+  font-weight: bold;
+}
+</style>
